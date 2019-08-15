589226309e4cdefdcd9fa5d68429b9374e25542a
Neal JIn Oh <no4259@gmail.com>
2019-08-15 15:40:54 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -37,9 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
-return programmer_hash[:alan_kay][:known_for]
 
 
 def dennis_ritchies_language
